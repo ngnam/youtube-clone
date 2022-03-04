a2d76fd3c06683eefede8879a5b4281440813ad6
ngnam <nguyenvannam0411@gmail.com>
2022-03-04 19:56:45 +0700

    c

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
+'use strict';
+
 console.clear();
 console.log(`Started at ${new Date()}`);
+
+function onView(e) {
+    const itemPreview = document.getElementById('item-preview');
+    let offsetTop = e.offsetTop,
+        offsetLeft = e.offsetLeft;
+
+    itemPreview.style.display = 'block';
+    itemPreview.style.top = `${offsetTop}px`;
+    itemPreview.style.left = `${offsetLeft}px`;
+};
+
+function onMouseleave(e) {
+    const itemPreview = document.getElementById('item-preview');
+    itemPreview.style.display = 'none';
+}
