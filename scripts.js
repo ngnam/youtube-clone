a746118d9086b36d31ebea0b77264cf9712c4948
ngnam <nguyenvannam0411@gmail.com>
2022-03-04 20:08:29 +0700

    commmit

--- scripts.js
+++ scripts.js
@@ -5,8 +5,9 @@ console.log(`Started at ${new Date()}`);
 
 function onView(e) {
     const itemPreview = document.getElementById('item-preview');
-    let offsetTop = e.offsetTop,
-        offsetLeft = e.offsetLeft;
+    const parentElement = e.parentElement.parentElement;
+    let offsetTop = parentElement.offsetTop,
+        offsetLeft = parentElement.offsetLeft;
 
     itemPreview.style.display = 'block';
     itemPreview.style.top = `${offsetTop}px`;
